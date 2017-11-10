b6d5df60e5bc22dfbc7679b14c458941e06526b5
Jack Koppa <hello@jackkoppa.com>
2017-11-09 19:21:27 -0600

    Simpler response object

--- 7-whats-in-a-query.js
+++ 7-whats-in-a-query.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/search', (req, res) => {
-    const response = {}
-    Object.keys(req.query).forEach(key => response[key] = req.query[key]);
+    const response = res.query;
     res.send(response);
 });
 app.listen(process.argv[2]);
