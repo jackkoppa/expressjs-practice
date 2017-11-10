0bcd000fed8670e52f1b3cbb5e15fca7ee6be02f
Jack Koppa <hello@jackkoppa.com>
2017-11-09 19:02:59 -0600

    Slightly improve variables for Exercise 5

--- 5-param-pam-pam.js
+++ 5-param-pam-pam.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 
 app.put('/message/:ID', (req, res) => {
+    const id = req.params.ID;
     const hash = crypto.createHash('sha1')
-        .update(new Date().toDateString() + req.params.ID)
+        .update(new Date().toDateString() + id)
         .digest('hex');
-    res.end(hash);
+    res.send(hash);
 });
 app.listen(process.argv[2]);
